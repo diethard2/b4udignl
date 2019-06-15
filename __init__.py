c1d872e8b76c79c6d96fe991d8cb24e25fe7d81d
Diethard Jansen <diethard.jansen@gmail.com>
2019-06-15 09:41:51 +0200

    solved problem of adding svg paths during first init op plugin

--- __init__.py
+++ __init__.py
@@ -26,13 +26,14 @@ from qgis.PyQt.QtCore import QSettings
 plugin_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(plugin_path)
 svg_path = os.path.join(plugin_path, 'styles', 'svg')
+# first check if we a settings exist for SVG paths, if not add it first!
 if not 'svg/searchPathsForSVG' in QSettings().allKeys():
     QSettings().setValue('svg/searchPathsForSVG', svg_path)
-else:
-    svg_paths = QSettings().value('svg/searchPathsForSVG')
-    if svg_path not in svg_paths:
-        svg_paths.append(svg_path)
-        QSettings().setValue('svg/searchPathsForSVG', svg_paths)
+# next check if path exists in setting, if not add it!
+svg_paths = QSettings().value('svg/searchPathsForSVG')
+if svg_path not in svg_paths:
+    svg_paths.append(svg_path)
+    QSettings().setValue('svg/searchPathsForSVG', svg_paths)
 
 def classFactory(iface):
     # load B4UdigNL class from file B4UdigNL
